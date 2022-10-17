536d752574f30901a4e2aaea46b308cd95583fda
KhanSaquib <saquibk894@gmail.com>
2022-10-18 00:55:40 +0530

    news

---
+++ src/components/NewsItem.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  render() {
+    let {title , description,imageUrl,url} =this.props
+    return (
+      <div>
+        <div className="card my-3" style={{width: "18rem"}}>
+          <img src={imageUrl} className="card-img-top" alt="" />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">
+              {description}
+            </p>
+            <a href={url} target='_blank' rel="noreferrer" className="btn btn-sm btn-primary">
+              Read More
+            </a>
+          </div>I
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsItem;
